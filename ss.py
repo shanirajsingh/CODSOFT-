d125c1d899b865564d35c6fb82f5cd55c1d39cf8
shanirajsingh <shanirajsinghchouhan@gmail.com>
2023-11-08 21:09:55 +0530

    add calulator.py

---
+++ ss.py
@@ -0,0 +1,8 @@
+# importing "math" for mathematical operations
+import math
+     
+x = 100
+     
+# returning the factorial
+print ("The factorial of 5 is : ", end ="")
+type(print(math.factorial(x)))
